dd2db584e766bdd8cf533625dbb17a76672e0c69
cccaaannn <can.kurt.aa@gmail.com>
2023-07-08 16:55:37 +0300

    config utils separated

--- src/config_utils.rs
+++ src/config_utils.rs
@@ -1,15 +1,47 @@
+use log::{error, Level};
 use serde::Deserialize;
 use std::fs;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
+struct AppConfigFile {
+    iteration: i32,
+    download_urls: Vec<String>,
+    log_level: String,
+}
+
+#[derive(Debug)]
 pub struct AppConfig {
     pub iteration: i32,
     pub download_urls: Vec<String>,
+    pub log_level: Level,
 }
 
-pub fn get_app_config() -> AppConfig {
-    let conf_str: String =
-        fs::read_to_string("config/config.toml").expect("Can not read config file");
-    let app_config: AppConfig = toml::from_str(&conf_str).expect("Can not read config file");
+lazy_static! {
+    pub static ref APP_CONFIG: AppConfig = get_app_config();
+}
+
+fn get_app_config() -> AppConfig {
+    let conf_str: String = fs::read_to_string("config/config.toml")
+        .map_err(|_| error!("Can not read config file"))
+        .unwrap();
+    let app_config_file: AppConfigFile = toml::from_str(&conf_str)
+        .map_err(|_| error!("Can not parse config file"))
+        .unwrap();
+
+    let level: Level = match app_config_file.log_level.as_str() {
+        "Error" => Level::Error,
+        "Warn" => Level::Warn,
+        "Info" => Level::Info,
+        "Debug" => Level::Debug,
+        "Trace" => Level::Trace,
+        _ => Level::Info,
+    };
+
+    let app_config: AppConfig = AppConfig {
+        iteration: app_config_file.iteration,
+        download_urls: app_config_file.download_urls,
+        log_level: level,
+    };
+
     app_config
 }
